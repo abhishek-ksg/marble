088cfc5c78092eedde654a6c18b195c9620e5d5d
abhishek-ksg <snowboyabhishek@gmail.com>
2018-03-02 16:07:16 +0530

    first cut for product list page

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'am-products',
+    templateUrl: './product-list.component.html'
+})
+export class ProductListComponent {
+
+}
