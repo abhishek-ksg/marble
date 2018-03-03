7b946f3e6a21c1463028af5f8a73f8caab1ca1c1
abhishek-ksg <snowboyabhishek@gmail.com>
2018-03-04 00:02:03 +0530

    first Cut of product Detail page with routing but always same dummy data

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -5,17 +5,20 @@ import { ProductService } from "./services/product.service";
 import { ProductListComponent } from "./products-list/product-list.component";
 import { SharedModule } from "../shared/shared.module";
 import { RouterModule } from "@angular/router";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
 
 @NgModule({
     imports: [
         HttpClientModule,
         SharedModule,
         RouterModule.forChild([
-            {path: 'products', component: ProductListComponent}
+            {path: 'products', component: ProductListComponent},
+            {path: 'products/:id', component: ProductDetailComponent}
         ])
     ],
     declarations: [
-        ProductListComponent
+        ProductListComponent,
+        ProductDetailComponent
     ],
     providers: [
         ProductService
