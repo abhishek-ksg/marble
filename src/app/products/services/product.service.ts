d76283202676d4fc71caf7d43af90fede8070c0f
abhishek-ksg <snowboyabhishek@gmail.com>
2018-03-10 00:10:59 +0530

    delay added to the product service getProducts function

--- src/app/products/services/product.service.ts
+++ src/app/products/services/product.service.ts
@@ -4,6 +4,7 @@ import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/delay';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/throw';
 
@@ -27,6 +28,7 @@ export class ProductService {
         }
         else {
             return this._http.get<IProduct[]>(this.productsUrl)
+            .delay(4000)
             .do( (products) => {
                 console.log(JSON.stringify(products))
                 this.cachedProduct = products;
