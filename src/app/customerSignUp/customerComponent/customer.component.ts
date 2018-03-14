5488dd047e0c388cda744052b68842f6b19f5681
abhishek-ksg <snowboyabhishek@gmail.com>
2018-03-15 01:17:44 +0530

    ability to add default data usigng setValue, also button added to form.

--- src/app/customerSignUp/customerComponent/customer.component.ts
+++ src/app/customerSignUp/customerComponent/customer.component.ts
@@ -10,7 +10,7 @@ import { Customer } from '../model/customer';
 })
 export class CustomerComponent implements OnInit {
 
-    public cusstomer: Customer = new Customer();
+    public customer: Customer = new Customer();
     public signUpForm: FormGroup;
 
     constructor(public fb: FormBuilder) { }
@@ -28,4 +28,21 @@ export class CustomerComponent implements OnInit {
         });
     }
 
+    fetchDefault() {
+        this.signUpForm.setValue({
+            firstName: 'Abhishek',
+            lastName: 'Jain',
+            email: 'abc@xyz',
+            confirmMail: 'abc@xyz',
+            phone: '9957608288',
+            notificationType: 'email',
+            rating: 3.5,
+            sendCatalog: true
+        });
+    }
+
+    submitSignUpForm() {
+        alert('Dude!!');
+    }
 }
+
