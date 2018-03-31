c85dbabec648aab4a119148217a590d8e0d7eb4e
abhishek-ksg <snowboyabhishek@gmail.com>
2018-03-31 17:45:44 +0530

    reactive form for edit info component, updating product object too - error exists in other related components

--- src/app/products/product-edit/product-edit-info/product-edit-info.component..ts
+++ src/app/products/product-edit/product-edit-info/product-edit-info.component..ts
@@ -1,8 +1,85 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { NumberValidators } from './../../../shared/validators/number.validators';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+import { GenericValidator } from './../../../shared/validators/generic.validator';
+import { IProduct } from './../../models/product.interface';
+import { FormGroup, FormControlName, FormBuilder, Validators } from '@angular/forms';
+import { Component, ViewChildren, ElementRef, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
     templateUrl: './product-edit-info.component.html'
 })
-export class ProductEditInfoComponent {
+export class ProductEditInfoComponent implements OnInit, AfterViewInit {
+    productInfoForm: FormGroup;
+    @ViewChildren(FormControlName, { read: ElementRef }) formInputElements: ElementRef[];
+    errMsg: {[key: string]: string} = {};
+    product: IProduct;
 
+    private validationErrMsgs: {[key: string]: {[key: string]: string}};
+    private genericValidator: GenericValidator;
+    private sub: Subscription;
+
+    constructor(private fb: FormBuilder,
+                private route: ActivatedRoute) {
+        this.validationErrMsgs = {
+            'productName': {
+                'required': 'Please enter the product name',
+                'minlength': 'Product Name should contains atleast 3 characters',
+            },
+            'productCode': {
+                'required': 'Please enter the product code'
+            },
+            'starRating': {
+                'required': 'Please enter the product rating',
+                'notANumber': 'Star Rating should be a number',
+                'lowOnRange': 'Star Rating should be atleast 1',
+                'outOfRange': 'Star Rating can be maximum 5'
+            }
+        };
+
+        this.genericValidator = new GenericValidator(this.validationErrMsgs);
+    }
+
+    ngOnInit() {
+        this.productInfoForm = this.fb.group({
+            productName: ['', [Validators.required,
+                                Validators.minLength(3)]],
+            productCode: ['', [Validators.required]],
+            starRating: ['', [Validators.required, NumberValidators.rangeValidator(1, 5)]],
+            description: ''
+        });
+
+        this.route.parent.data.subscribe( (data) => {
+            this.onProductDataReceived(data['product']);
+        });
+    }
+
+    ngAfterViewInit() {
+        // Watch for the blur event from any input element on the form.
+        const controlBlurs: Observable<any>[] = this.formInputElements
+            .map((formControl: ElementRef) => Observable.fromEvent(formControl.nativeElement, 'blur'));
+
+        // Merge the blur event observable with the valueChanges observable
+        Observable.merge(this.productInfoForm.valueChanges, ...controlBlurs).debounceTime(800).subscribe(value => {
+            this.errMsg = this.genericValidator.processMessages(this.productInfoForm);
+            // Updating the product object
+            Object.assign(this.product, this.productInfoForm.value);
+        });
+    }
+
+    private onProductDataReceived(product: IProduct): void {
+        if (this.productInfoForm) {
+            this.productInfoForm.reset();
+        }
+
+        this.product = product;
+
+        this.productInfoForm.patchValue({
+            productName: this.product.productName,
+            productCode: this.product.productCode,
+            starRating: this.product.starRating,
+            description: this.product.description
+        });
+    }
 }
--- src/app/products/product-edit/product-edit-parent/product-edit-parent.component.ts
+++ src/app/products/product-edit/product-edit-parent/product-edit-parent.component.ts
@@ -1,9 +1,97 @@
-import { Component } from '@angular/core';
+import { IProduct } from './../../models/product.interface';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { NumberValidators } from './../../../shared/validators/number.validators';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChildren } from '@angular/core';
+import { GenericValidator } from '../../../shared/validators/generic.validator';
+import { Subscription } from 'rxjs/Subscription';
+
+import { ProductService } from './../../services/product.service';
 
 @Component({
     templateUrl: './product-edit-parent.component.html',
     styleUrls: ['./product-edit-parent.component.css']
 })
-export class ProductEditParentComponent {
+export class ProductEditParentComponent implements OnInit {
 
+    productDataErr: string;
+    pageTitle: string = 'Add Product';
+
+    private currentProduct: IProduct;
+    private originalProduct: IProduct;
+
+    constructor(private route: ActivatedRoute,
+                private router: Router,
+                private productService: ProductService) {
+    }
+
+    get product(): IProduct {
+        return this.currentProduct;
+    }
+    set product(value: IProduct) {
+        this.currentProduct = value;
+        // Clone the object to retain a copy
+        this.originalProduct = Object.assign({}, value);
+    }
+
+
+    ngOnInit() {
+        this.route.data.subscribe( (data) => {
+            this.onProductDataReceived(data['product']);
+        });
+    }
+
+    saveProduct() {
+        // if (this.productForm.dirty && this.productForm.valid) {
+        //     const newProduct = Object.assign({}, this.product, this.productForm.value);
+        //     this.productService.saveProduct(newProduct)
+        //         .subscribe(
+        //             (product: IProduct) => this.onSaveComplete(),
+        //             (err) => this.productDataErr = <any>err
+        //         );
+        // } else if (!this.productForm.dirty) {
+        //     this.onSaveComplete();
+        // }
+    }
+
+    deleteProduct() {
+        if (this.product.id === 0) {
+            // Don't delete, it was never saved.
+            this.onSaveComplete();
+        } else {
+            if (confirm(`Really delete the product: ${this.product.productName}?`)) {
+                this.productService.deleteProduct(this.product.id)
+                    .subscribe(
+                        () => this.onSaveComplete(),
+                        (error: any) => this.productDataErr = <any>error
+                    );
+            }
+        }
+    }
+
+    private onSaveComplete(): void {
+        this.router.navigate(['/products'], {queryParamsHandling: 'preserve'});
+    }
+
+    private getProductData(productId: number): void {
+        this.productService.getProductData(productId)
+            .subscribe( (product: IProduct) => this.onProductDataReceived(product),
+                        ( error: any ) => this.productDataErr = <any>error);
+    }
+
+    private onProductDataReceived(product: IProduct): void {
+        this.product = product;
+
+        if (this.product.id === 0) {
+            this.pageTitle = 'Add Product';
+        } else {
+            this.pageTitle = 'Edit Product: ' + this.product.productName;
+        }
+
+        this.productDataErr = '';
+    }
+
+    cancelProductEdit(): void {
+        this.router.navigate(['/products'], {queryParamsHandling: 'preserve'});
+    }
 }
